02d6aee042078442198aa27fd37cbd4528d5e5c5
mzambranob <157537081+mzambranob@users.noreply.github.com>
2025-04-06 20:34:06 -0500

    Add files via upload

---
+++ dist/main.js
@@ -0,0 +1,44 @@
+import { dataSeries } from './data.js';
+const seriesTbody = document.getElementById("series");
+const avgSeasons = document.getElementById("avgSeasons");
+const cardContainer = document.getElementById("card-container");
+function showSeriesInTable(series) {
+    series.forEach((s) => {
+        const trElement = document.createElement("tr");
+        trElement.innerHTML = `
+        <td>${s.tupleNum}</td>
+        <td> <a href="#"> ${s.name} </a> </td>
+        <td>${s.channel}</td>
+        <td>${s.seasons}</td>
+        `;
+        trElement.addEventListener("click", () => {
+            showCardDetail(s);
+        });
+        seriesTbody.appendChild(trElement);
+    });
+}
+function showCardDetail(serie) {
+    cardContainer.innerHTML = `
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="${serie.image}" alt="Poster of ${serie.name}">
+      <div class="card-body">
+        <h5 class="card-title">${serie.name}</h5>
+        <p class="card-text">
+          ${serie.desc}
+        </p>
+        <p>
+        <a href="${serie.link}" target="_blank">
+          ${serie.link}
+        </a>
+        </p>
+      </div>
+    </div>
+  `;
+}
+function getAverageSeasons(series) {
+    const total = series.reduce((acc, series) => acc + series.seasons, 0);
+    return total / series.length;
+}
+showSeriesInTable(dataSeries);
+const average = getAverageSeasons(dataSeries);
+avgSeasons.innerText = average.toFixed(2);
---
+++ dist/serie.js
@@ -0,0 +1,11 @@
+export class Serie {
+    constructor(name, channel, seasons, tupleNum, desc, image, link) {
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.tupleNum = tupleNum;
+        this.desc = desc;
+        this.image = image;
+        this.link = link;
+    }
+}
